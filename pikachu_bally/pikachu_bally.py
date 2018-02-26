2460ed8cb8cacb637192ab49819a162dbeceb87d
parkchu <parkjoohan0603@gmail.com>
2018-02-26 14:36:15 +0900

    move pikachu and ball

--- pikachu_bally/pikachu_bally.py
+++ pikachu_bally/pikachu_bally.py
@@ -11,32 +11,43 @@ class Pikachu(pygame.sprite.Sprite):
 
     def move(self):
         self.rect = self.rect.move(self.speed)
-        if self.rect.left < 0 or self.rect.right > 640:
-            self.speed[0] = self.speed[0] * 0
-
+        if self.rect.left < 0 or self.rect.right > 1034:
+            self.speed[0] = - self.speed[0]
+        if self.rect.bottom >= 625 - 65:
+            self.speed[1] = self.speed[1] * 0
+            a = 0
+            return a
+        if joohan == 0:
+            if self.rect.top < 0:
+                self.speed[0] = - self.speed[0]
+                self.speed[1] = - self.speed[1]
 
 pygame.init()
 screen = pygame.display.set_mode([1034,625])
 screen.fill([255,255,255])
 
-speed = 0,0
+speed = [0,0]
 location = 0,0
 background = Pikachu(speed, location, 'background.png')
 
+speed = [0,0]
 location = 1034/2 - 25, 625 - 290
 stick = Pikachu(speed, location, 'stick.png')
 
-location = 0, 625 - 240
+speed = [0,0]
+location = 100, 625 - 240
 left = Pikachu(speed, location, 'leftpikachu.png')
 
-location = 1034 - 142, 625 - 240
+speed = [0,0]
+location = 1034 - 242, 625 - 240
 right = Pikachu(speed, location, 'rightpikachu.png')
 
+speed = [0,0]
 location = 0, 625 - 500
 jump = Pikachu(speed, location, 'jump.png')
 
-speed = 0, 0
-location = 0, 0
+speed = [0, 5]
+location = 100, - 50
 monsterball = Pikachu(speed, location, 'monsterball.png')
 
 screen.blit(background.image, background.rect)
@@ -46,36 +57,90 @@ screen.blit(right.image, right.rect)
 screen.blit(jump.image, jump.rect)
 screen.blit(monsterball.image, monsterball.rect)
 pygame.display.flip()
-
+delay = 100
+interval = 50
+pygame.key.set_repeat(delay, interval)
+joohan = 1
 
 while True:
     screen.blit(background.image, background.rect)
     screen.blit(stick.image, stick.rect)
 
-
+    a = right.move()
+    b = left.move()
+    monsterball.move()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                right.speed[1] = right.speed[1] - 3
+                if a == 0:
+                    right.speed[1] = -10
+                    right.move()
+                    a = 1
             if event.key == pygame.K_LEFT:
-                right.speed[0] = right.speed[0] - 2
+                right.speed[0] = - 5
+                right.move()
             if event.key == pygame.K_RIGHT:
-                right.speed[0] = right.speed[0] + 2
+                right.speed[0] = 5
+                right.move()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                if b == 0:
+                    left.speed[1] = -10
+                    left.move()
+                    b = 1
+            if event.key == pygame.K_a:
+                left.speed[0] = - 5
+                left.move()
+            if event.key == pygame.K_d:
+                left.speed[0] = 5
+                left.move()
+
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                right.speed[1] = right.speed[1] * 0
             if event.key == pygame.K_LEFT:
                 right.speed[0] = right.speed[0] * 0
+                right.move()
             if event.key == pygame.K_RIGHT:
                 right.speed[0] = right.speed[0] * 0
+                right.move()
 
-        #    leftpikachu.move()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                left.speed[0] =  0
+                left.move()
+            if event.key == pygame.K_d:
+                left.speed[0] = 0
+                left.move()
 
+    if pygame.sprite.collide_rect(right, stick):
+        right.speed[0] = - right.speed[0]
+
+    if pygame.sprite.collide_rect(right, jump):
+        right.speed[1] = - right.speed[1]
+
+    if pygame.sprite.collide_rect(left, stick):
+        left.speed[0] = - left.speed[0]
+
+    if pygame.sprite.collide_rect(left, jump):
+        left.speed[1] = - left.speed[1]
+
+    if pygame.sprite.collide_rect(monsterball, left):
+        monsterball.speed[0] = - monsterball.speed[0]
+        monsterball.speed[1] = - monsterball.speed[1]
+        joohan = 0
+
+    if pygame.sprite.collide_rect(monsterball, right):
+        monsterball.speed[0] = - monsterball.speed[0]
+        monsterball.speed[1] = - monsterball.speed[1]
+        joohan = 0
+
+    right.move()
+    left.move()
     monsterball.move()
     screen.blit(monsterball.image, monsterball.rect)
-    right.move()
+
     screen.blit(right.image, right.rect)
     screen.blit(left.image, left.rect)
     pygame.display.flip()
