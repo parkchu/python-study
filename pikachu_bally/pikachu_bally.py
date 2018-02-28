491d68d1fb9c4379b8649c7ea5ac0ad1e4bc942d
parkchu <parkjoohan0603@gmail.com>
2018-02-28 16:07:04 +0900

    move ball

--- pikachu_bally/pikachu_bally.py
+++ pikachu_bally/pikachu_bally.py
@@ -19,7 +19,6 @@ class Pikachu(pygame.sprite.Sprite):
             return a
         if joohan == 0:
             if self.rect.top < 0:
-                self.speed[0] = - self.speed[0]
                 self.speed[1] = - self.speed[1]
 
 pygame.init()
@@ -31,8 +30,12 @@ location = 0,0
 background = Pikachu(speed, location, 'background.png')
 
 speed = [0,0]
-location = 1034/2 - 25, 625 - 290
-stick = Pikachu(speed, location, 'stick.png')
+location = 1034/2 - 25, 650 - 290
+stick = Pikachu(speed, location, 'stickbody.png')
+
+speed = [0,0]
+location = 1034/2 - 25, 650 - 315
+stickhead = Pikachu(speed, location, 'sticktop.png')
 
 speed = [0,0]
 location = 100, 625 - 240
@@ -56,6 +59,8 @@ screen.blit(left.image, left.rect)
 screen.blit(right.image, right.rect)
 screen.blit(jump.image, jump.rect)
 screen.blit(monsterball.image, monsterball.rect)
+screen.blit(stickhead.image, stickhead.rect)
+
 pygame.display.flip()
 delay = 100
 interval = 50
@@ -65,7 +70,7 @@ joohan = 1
 while True:
     screen.blit(background.image, background.rect)
     screen.blit(stick.image, stick.rect)
-
+    screen.blit(stickhead.image, stickhead.rect)
     a = right.move()
     b = left.move()
     monsterball.move()
@@ -81,6 +86,8 @@ while True:
             if event.key == pygame.K_LEFT:
                 right.speed[0] = - 5
                 right.move()
+
+
             if event.key == pygame.K_RIGHT:
                 right.speed[0] = 5
                 right.move()
@@ -94,6 +101,7 @@ while True:
             if event.key == pygame.K_a:
                 left.speed[0] = - 5
                 left.move()
+
             if event.key == pygame.K_d:
                 left.speed[0] = 5
                 left.move()
@@ -115,26 +123,41 @@ while True:
                 left.move()
 
     if pygame.sprite.collide_rect(right, stick):
-        right.speed[0] = - right.speed[0]
+        right.speed[0] = 5
 
     if pygame.sprite.collide_rect(right, jump):
-        right.speed[1] = - right.speed[1]
+        right.speed[1] = 10
 
     if pygame.sprite.collide_rect(left, stick):
-        left.speed[0] = - left.speed[0]
+        left.speed[0] = - 5
 
     if pygame.sprite.collide_rect(left, jump):
-        left.speed[1] = - left.speed[1]
+        left.speed[1] = 10
 
     if pygame.sprite.collide_rect(monsterball, left):
-        monsterball.speed[0] = - monsterball.speed[0]
         monsterball.speed[1] = - monsterball.speed[1]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                monsterball.speed[0] = monsterball.speed[0] - 1
+            if event.key == pygame.K_d:
+                monsterball.speed[0] = monsterball.speed[0] + 1
+
         joohan = 0
 
     if pygame.sprite.collide_rect(monsterball, right):
-        monsterball.speed[0] = - monsterball.speed[0]
         monsterball.speed[1] = - monsterball.speed[1]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                monsterball.speed[0] = monsterball.speed[0] - 1
+            if event.key == pygame.K_RIGHT:
+                monsterball.speed[0] = monsterball.speed[0] + 1
+
         joohan = 0
+
+    if pygame.sprite.collide_rect(monsterball, stickhead):
+        monsterball.speed[1] = - monsterball.speed[1]
+    if pygame.sprite.collide_rect(monsterball, stick):
+        monsterball.speed[0] = - monsterball.speed[0]
 
     right.move()
     left.move()
